2c309dc3388ecedd893d3fd1d1ab430feca04ea1
Ignacio <ignacionazar9@gmail.com>
2025-05-16 16:11:48 -0300

    agregue el boton para que cada elemento ingresado en la lista debe poder ser eliminado

--- ejercicio3.js
+++ ejercicio3.js
@@ -9,6 +9,14 @@ const agregarTarea = (e) => {
   tareaLista.textContent = tarea.value;
   tareaLista.classList.add("list-group-item");
 
+  const boton = document.createElement("button");
+  boton.innerHTML = `<i class="bi bi-trash"></i>`;
+
+  boton.addEventListener("click", () => {
+    ulTarea.removeChild(tareaLista);
+  });
+  
+  tareaLista.appendChild(boton);
   ulTarea.appendChild(tareaLista);
   ulTarea.classList.remove("d-none");
   tarea.value = "";
